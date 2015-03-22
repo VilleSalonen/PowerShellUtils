2a48c578e41d235dde65112b970b063e32705821
Ville Salonen <ville.salonen@iki.fi>
2015-03-22 13:13:34 +0200

    Added Enable-ExplorerPowerShellShortcuts.ps1

---
+++ Enable-ExplorerPowerShellShortcuts.ps1
@@ -0,0 +1,16 @@
+﻿# Add "Open PowerShell here" shortcuts to Windows Explorer context menus in
+# current directory and currently selected directory.
+
+$pspath = "$PSHome\powershell.exe -Noexit -Nologo"
+
+# Add shortcut for selected directory
+New-Item HKLM:\SOFTWARE\Classes\Directory\shell\powershell -force
+Set-Item HKLM:\SOFTWARE\Classes\Directory\shell\powershell "Open PowerShell Here"
+New-item HKLM:\SOFTWARE\Classes\Directory\shell\powershell\command -force
+Set-item HKLM:\SOFTWARE\Classes\Directory\shell\powershell\command "$pspath -Command Set-Location '%L'"
+
+# Add shortcut for current directory
+New-Item HKLM:\SOFTWARE\Classes\Directory\Background\shell\powershell -force
+Set-Item HKLM:\SOFTWARE\Classes\Directory\Background\shell\powershell "Open PowerShell Here"
+New-item HKLM:\SOFTWARE\Classes\Directory\Background\shell\powershell\command -force
+Set-item HKLM:\SOFTWARE\Classes\Directory\Background\shell\powershell\command "$pspath -Command Set-Location '%V'"
